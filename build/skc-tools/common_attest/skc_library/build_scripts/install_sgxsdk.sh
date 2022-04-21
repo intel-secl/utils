02362672d7e4b850a552b45331cc150c27d6ab22
afathimx <95069537+afathimx@users.noreply.github.com>
2022-04-21 19:16:27 +0530

    ISECL-21006 : SGX SDK installation failed during v5GA  Ubuntu container build (#649)

--- build/skc-tools/common_attest/skc_library/build_scripts/install_sgxsdk.sh
+++ build/skc-tools/common_attest/skc_library/build_scripts/install_sgxsdk.sh
@@ -7,7 +7,12 @@ fi
 
 install_sgxsdk()
 {
-	wget -q $SGX_URL_TEE/sgx_linux_x64_sdk_$SGX_SDK_VERSION_TEE.bin || exit 1
+
+        if [ "$OS" == "rhel" ]; then
+                wget -q $SGX_URL_TEE/sgx_linux_x64_sdk_$SGX_SDK_VERSION_TEE.bin || exit 1
+        elif [ "$OS" == "ubuntu" ]; then
+                wget -q $SGX_URL_TEE_UB/sgx_linux_x64_sdk_$SGX_SDK_VERSION_TEE.bin || exit 1
+        fi
         chmod u+x sgx_linux_x64_sdk_$SGX_SDK_VERSION_TEE.bin
         ./sgx_linux_x64_sdk_$SGX_SDK_VERSION_TEE.bin -prefix=$SGX_INSTALL_DIR || exit 1
         source $SGX_INSTALL_DIR/sgxsdk/environment
@@ -15,7 +20,7 @@ install_sgxsdk()
                 echo "${red} failed while setting sgx environment ${reset}"
                 exit 1
         fi
-	rm -f sgx_linux_x64_sdk_$SGX_SDK_VERSION_TEE.bin
+        rm -rf sgx_linux_x64_sdk_$SGX_SDK_VERSION_TEE.bin
 }
 
 install_sgxsdk
