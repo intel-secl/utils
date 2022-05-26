5065bd6c5fab89dcd571db7e7c4a6249a50e005d
srinics <srinivasan.rajendran@intel.com>
2022-05-26 18:55:13 +0530

    ISECL-21302: Uploaded image for project: 'ISecL' ISecLISECL-21302 Update platform-dependency script with latest DCAP release (#659)
    * ISECL-21302: Updated pre-req script with latest dcap

--- build/skc-tools/common_attest/tee/build_scripts/install_prereqs.sh
+++ build/skc-tools/common_attest/tee/build_scripts/install_prereqs.sh
@@ -12,7 +12,7 @@ install_prereqs()
 {
         if [ "$OS" == "rhel" ]; then
 		$PKGMGR install -qy https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm || exit 1
-                $PKGMGR install -y http://rpmfind.net/linux/fedora/linux/releases/32/Everything/x86_64/os/Packages/m/msr-tools-1.3-13.fc32.x86_64.rpm || exit 1
+                $PKGMGR install -y msr-tools || exit 1
 		$PKGMGR install -y yum-utils gcc-c++ git wget make kernel-devel kernel-headers dkms tar curl-devel || exit 1
         elif [ "$OS" == "ubuntu" ]; then
 		$PKGMGR install -y git wget make dkms tar libcurl4-openssl-dev || exit 1
@@ -73,7 +73,7 @@ uninstall()
 {
         echo "uninstalling sgx psw/qgl and multi-package agent rpm"
         if [ "$OS" == "rhel" ]; then
-                $PKGMGR remove -y libsgx-ra-uefi libsgx-launch libsgx-uae-service libsgx-urts libsgx-ae-qve libsgx-dcap-ql libsgx-dcap-ql-devel libsgx-dcap-default-qpl-devel libsgx-dcap-default-qpl
+                $PKGMGR remove -y libsgx-ra-uefi libsgx-urts libsgx-ae-qve libsgx-dcap-ql libsgx-dcap-ql-devel libsgx-dcap-default-qpl-devel libsgx-dcap-default-qpl libsgx-ae-pce libsgx-enclave-common libsgx-pce-logic libsgx-headers libsgx-dcap-quote-verify libsgx-dcap-quote-verify-devel sgx-pck-id-retrieval-tool || exit 1
         elif [ "$OS" == "ubuntu" ]; then
                 $PKGMGR remove -y libsgx-ra-uefi libsgx-launch libsgx-uae-service libsgx-urts libsgx-ae-qve libsgx-dcap-ql libsgx-dcap-ql-dev libsgx-dcap-default-qpl-dev libsgx-dcap-default-qpl
         fi
@@ -84,15 +84,17 @@ uninstall()
                 if [[ "$DRIVER_LOADED" -ne 0 ]]; then
                         echo "SGX DCAP driver not installed"
                 elif [ "$DRIVER_VERSION" != "$SGX_DRIVER_VERSION" ]; then
-                        echo "uninstalling sgx dcap driver"
-                        systemctl stop aesmd
-                        sh $SGX_INSTALL_DIR/sgxdriver/uninstall.sh
-                        if [[ $? -ne 0 ]]; then
-                                echo "${red} sgx dcap driver uninstallation failed. exiting ${reset}"
-                                exit 1
-                        fi
-                        systemctl start aesmd
-                fi
+			echo "uninstalling sgx dcap driver"
+			systemctl stop aesmd
+			sh $SGX_INSTALL_DIR/sgxdriver/uninstall.sh
+			if [[ $? -ne 0 ]]; then
+				echo "${red} sgx dcap driver uninstallation failed. exiting ${reset}"
+				exit 1
+			fi
+			systemctl start aesmd
+		else
+                	echo "Skipping uninstallation of sgx dcap driver"
+		fi
         else
                 echo " $SGX_DRIVER_VERSION found inbuilt sgx driver, skipping dcap driver uninstallation"
         fi
@@ -102,7 +104,7 @@ install_sgxsdk()
 {
 
 	if [ "$OS" == "rhel" ]; then
-        	wget -q $SGX_URL_TEE/sgx_linux_x64_sdk_$SGX_SDK_VERSION_TEE.bin || exit 1
+        	wget -q $SGX_URL_TEE_UB/sgx_linux_x64_sdk_$SGX_SDK_VERSION_TEE.bin || exit 1
 	elif [ "$OS" == "ubuntu" ]; then
 		wget -q $SGX_URL_TEE_UB/sgx_linux_x64_sdk_$SGX_SDK_VERSION_TEE.bin || exit 1
 	fi
@@ -122,9 +124,9 @@ install_dcap_driver()
         chmod u+x sgx_linux_x64_driver_${SGX_DRIVER_VERSION}.bin
         if [[ "$INKERNEL_SGX" -eq 1 ]]; then
                 if [[ "$DRIVER_VERSION" == ""  || "$DRIVER_VERSION" != "$SGX_DRIVER_VERSION" ]]; then
-                        echo "Installing sgx dcap driver...."
-                        ./sgx_linux_x64_driver_${SGX_DRIVER_VERSION}.bin -prefix=$SGX_INSTALL_DIR || exit 1
-                        echo "${green} sgx dcap driver installed successfully ${reset}"
+			echo "Installing sgx dcap driver...."
+			./sgx_linux_x64_driver_${SGX_DRIVER_VERSION}.bin -prefix=$SGX_INSTALL_DIR || exit 1
+			echo "${green} sgx dcap driver installed successfully ${reset}"
                 elif [ "$DRIVER_VERSION" != "$SGX_DRIVER_VERSION" ]; then
                         echo "${red} incompatible sgx dcap driver loaded, uninstall the existing driver before proceeding ${reset}"
                         exit 1
@@ -138,13 +140,13 @@ install_dcap_driver()
 install_multipackage_agent_rpm()
 {
         if [ "$OS" == "rhel" ]; then
-		rm -rf sgx_rpm_local_repo sgx_rpm_local_repo.tgz
-                wget -q $MPA_URL/sgx_rpm_local_repo.tgz -O - | tar -xz || exit 1
-                pushd $PWD
-		cd sgx_rpm_local_repo
-		rpm -ivh libsgx-ra-uefi-$MP_RPM_VER.el8.x86_64.rpm
-		popd
-                rm -rf sgx_rpm_local_repo sgx_rpm_local_repo.tgz
+		rm -rf sgx_rpm_local_repo sgx_rpm_local_repo.tar.gz
+		wget -q $SGX_URL_TEE_UB/sgx_rpm_local_repo.tar.gz -O - | tar -xz || exit 1
+                yum-config-manager --add-repo file://$PWD/sgx_rpm_local_repo || exit 1
+		$PKGMGR install -qy --nogpgcheck libsgx-ra-uefi || exit 1
+                
+		rm -rf sgx_rpm_local_repo sgx_rpm_local_repo.tar.gz /etc/yum.repos.d/*sgx_rpm_local_repo.repo
+                rm -rf sgx_rpm_local_repo sgx_rpm_local_repo.tar.gz
         elif [ "$OS" == "ubuntu" ]; then
 		wget -q https://download.01.org/intel-sgx/sgx-dcap/$DCAP_VERSION/linux/tools/SGXMultiPackageAgent/$OS_FLAVOUR-server/debian_pkgs/libs/libsgx-ra-uefi/libsgx-ra-uefi_1.12.100.3-focal1_amd64.deb || exit 1
 			$PKGMGR install -y libsgx-ra-uefi
@@ -157,12 +159,18 @@ install_multipackage_agent_rpm()
 install_psw_qgl()
 {
         if [ "$OS" == "rhel" ]; then
-		rm -rf sgx_rpm_local_repo sgx_rpm_local_repo.tgz
-		wget -q $SGX_URL/sgx_rpm_local_repo.tgz || exit 1
-                tar -xf sgx_rpm_local_repo.tgz || exit 1
+		pushd $PWD
+                cd /opt/intel/
+                wget -q $SGX_URL_TEE_UB/libsgx_dcap_quoteverify.so.sbx || exit 1
+                popd
+		wget -q $SGX_URL_TEE_UB/libPCKCertSelection.so || exit 1
+		\cp -pf libPCKCertSelection.so /usr/lib64/libPCKCertSelection.so
+		rm -rf sgx_rpm_local_repo sgx_rpm_local_repo.tar.gz
+		wget -q $SGX_URL_TEE_UB/sgx_rpm_local_repo.tar.gz || exit 1
+                tar -xf sgx_rpm_local_repo.tar.gz || exit 1
                 yum-config-manager --add-repo file://$PWD/sgx_rpm_local_repo || exit 1
-                $PKGMGR install -qy --nogpgcheck libsgx-launch libsgx-uae-service libsgx-urts libsgx-ae-qve libsgx-dcap-ql libsgx-dcap-ql-devel libsgx-dcap-default-qpl-devel libsgx-dcap-default-qpl || exit 1
-                rm -rf sgx_rpm_local_repo sgx_rpm_local_repo.tgz /etc/yum.repos.d/*sgx_rpm_local_repo.repo
+                $PKGMGR install -qy --nogpgcheck libsgx-urts libsgx-ae-qve libsgx-dcap-ql libsgx-dcap-ql-devel libsgx-dcap-default-qpl-devel libsgx-dcap-default-qpl libsgx-ae-pce libsgx-enclave-common libsgx-pce-logic libsgx-headers libsgx-dcap-quote-verify libsgx-dcap-quote-verify-devel || exit 1
+                rm -rf sgx_rpm_local_repo sgx_rpm_local_repo.tar.gz /etc/yum.repos.d/*sgx_rpm_local_repo.repo
         elif [ "$OS" == "ubuntu" ]; then
                 echo $SGX_LIBS_REPO | sudo tee /etc/apt/sources.list.d/intel-sgx.list
                 wget -qO - https://download.01.org/intel-sgx/sgx_repo/ubuntu/intel-sgx-deb.key | sudo apt-key add -
@@ -176,19 +184,32 @@ install_psw_qgl()
 
 install_pckretrieval_tool()
 {
-	rm -rf $GIT_CLONE_PATH
-        pushd $PWD
-        git clone $SGX_DCAP_REPO $GIT_CLONE_PATH || exit 1
-        cp -pf remove_pccs_connect.diff $GIT_CLONE_PATH/tools/PCKRetrievalTool
-        cd $GIT_CLONE_PATH/
-        git checkout $SGX_DCAP_TAG
-        cd $GIT_CLONE_PATH/tools/PCKRetrievalTool
-        git apply remove_pccs_connect.diff
-        make || exit 1
-        \cp -pf libdcap_quoteprov.so.1 pck_id_retrieval_tool_enclave.signed.so $USR_BIN_DIR
-        \cp -pf PCKIDRetrievalTool $USR_BIN_DIR
-        rm -rf $GIT_CLONE_PATH
-        popd
+        if [ "$OS" == "rhel" ]; then
+		rm -rf sgx_rpm_local_repo sgx_rpm_local_repo.tar.gz
+		wget -q $SGX_URL_TEE_UB/sgx_rpm_local_repo.tar.gz || exit 1
+                tar -xf sgx_rpm_local_repo.tar.gz || exit 1
+                yum-config-manager --add-repo file://$PWD/sgx_rpm_local_repo || exit 1
+		$PKGMGR install -qy --nogpgcheck sgx-pck-id-retrieval-tool  || exit 1
+		cp /opt/intel/sgx-pck-id-retrieval-tool/PCKIDRetrievalTool /usr/bin/
+		cp /opt/intel/sgx-pck-id-retrieval-tool/libsgx_id_enclave.signed.so /usr/bin/
+		cp /opt/intel/sgx-pck-id-retrieval-tool/libsgx_pce.signed.so /usr/bin/
+                rm -rf sgx_rpm_local_repo sgx_rpm_local_repo.tar.gz /etc/yum.repos.d/*sgx_rpm_local_repo.repo
+        elif [ "$OS" == "ubuntu" ]; then
+		rm -rf $GIT_CLONE_PATH
+		pushd $PWD
+		git clone $SGX_DCAP_REPO $GIT_CLONE_PATH || exit 1
+		cp -pf remove_pccs_connect.diff $GIT_CLONE_PATH/tools/PCKRetrievalTool
+		cd $GIT_CLONE_PATH/
+		git checkout $SGX_DCAP_TAG
+		cd $GIT_CLONE_PATH/tools/PCKRetrievalTool
+		git apply remove_pccs_connect.diff
+		make || exit 1
+		\cp -pf libdcap_quoteprov.so.1 pck_id_retrieval_tool_enclave.signed.so $USR_BIN_DIR
+		\cp -pf PCKIDRetrievalTool $USR_BIN_DIR
+		rm -rf $GIT_CLONE_PATH
+        	popd
+	fi
+
         echo "${green} pckid retrieval tool installed ${reset}"
 	PCKIDRetrievalTool | grep -Iw "pckid_retrieval.csv has been generated successfully!"
 	if [ $? -ne 0 ]; then
